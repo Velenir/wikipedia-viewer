4afdfb2e2d02f540c4022f7df55e0c5438d9c473
Velenir <Velenir@users.noreply.github.com>
2016-07-03 16:01:14 +0300

    Ajax request working

--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
 const ex = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cpageimages&generator=search&exsentences=2&exlimit=10&exintro=1&explaintext=1&piprop=thumbnail&pithumbsize=300&pilimit=10&gsrnamespace=0&gsrlimit=10&gsrsearch=";
+const min = "https://en.wikipedia.org/w/api.php";
 
 
 // $.ajax({
@@ -14,12 +15,29 @@ const ex = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=ext
 // 		}
 // }) // end ajax
 
+const resultsCount = 10, thumbnailsize = 300;
+
 function getSearchResults(term, cb) {
 	$.ajax({
-		url: ex + term,
-		method: 'GET',
-		dataType: 'jsonp',
-		data: {param1: 'value1'}
+		url: min,
+		method: "GET",
+		dataType: "jsonp",
+		data: {
+			action: "query",
+			format: "json",
+			generator: "search",
+			gsrnamespace: 0,
+			gsrlimit: resultsCount,
+			gsrsearch: term,
+			prop: "extracts|pageimages",
+			exsentences: 2,
+			exlimit: resultsCount,
+			exintro: 1,
+			explaintext: 1,
+			piprop: "thumbnail",
+			pithumbsize: thumbnailsize,
+			pilimit: resultsCount
+		}
 	})
 	.then(cb, function (_, status, error) {
 		console.log("Error", error, ", status", status);
@@ -27,7 +45,13 @@ function getSearchResults(term, cb) {
 }
 
 function logResult() {
-	getSearchResults("Time", data => console.log(data));
+	getSearchResults("Time", data => {
+		const pages = data.query.pages;
+		for (let key of Object.keys(pages)) {
+			let {title, pageid, extract, thumbnail} = pages[key];
+			console.log(key, "title:", title, ", pageid:", pageid, ", extract:", extract, ", img:", thumbnail ? thumbnail.source : null);
+		}
+	});
 }
 
 const $input = $("#search-input");
